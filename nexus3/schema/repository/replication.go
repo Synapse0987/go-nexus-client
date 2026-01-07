58395091dc8727fbe1966220e5b093da81800cda
Eugene Teh (HTX) <eugene_teh@htx.gov.sg>
2026-01-07 22:21:10 +0800

    test

--- nexus3/schema/repository/replication.go
+++ nexus3/schema/repository/replication.go
@@ -1,6 +1,6 @@
 package repository
 
 type Replication struct {
-	enabled bool `json:"preemptivePullEnabled"`
-	path_regex *string `json:"assetPathRegex,omitempty"`
+	PreemptivePullEnabled bool    `json:"preemptivePullEnabled"`
+	AssetPathRegex        *string `json:"assetPathRegex,omitempty"`
 }
